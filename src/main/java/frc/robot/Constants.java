b1770eaf08dc447725a767e22f5d69674bc5fa1a
FreeJuicebox <60396768+FreeJuicebox@users.noreply.github.com>
2023-02-24 15:41:53 -0600

    value changes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,9 +5,9 @@ import edu.wpi.first.math.util.Units;
 import swervelib.parser.PIDFConfig;
 
 public final class Constants {
-  public static final double ROBOT_MASS = (148-20.3) * 0.453592; //32lbs * kg per pound
+  public static final double ROBOT_MASS = (135) * 0.453592; //32lbs * kg per pound
   public static final double CHASSIS_MASS = ROBOT_MASS;
-  public static final Translation3d CHASSIS_CG = new Translation3d(0, 0, Units.inchesToMeters(8));
+  public static final Translation3d CHASSIS_CG = new Translation3d(0, 0, Units.inchesToMeters(10));
   public static final double LOOP_TIME = 0.13; //s, 20ms + 110ms spark max velocity lag
 
 
